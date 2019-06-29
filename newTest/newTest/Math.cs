f3924beecaf5b94b0edca6e2d0d5bb2961df2bc6
pavelkarpovich2015 <pavelkarpovich2015@gmail.com>
2019-06-29 15:37:29 +0300

    Class Math is added

--- newTest/newTest/Math.cs
+++ newTest/newTest/Math.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace newTest
 {
-    public class Class1
+    public class Math
     {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
